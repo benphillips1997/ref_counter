247e22cbc6f778f500f9d79088f520f21ae55c00
Ben Phillips <ben3.phillips@live.uwe.ac.uk>
2023-11-08 03:03:39 +0000

    Separated test code into functions

--- test_string.cpp
+++ test_string.cpp
@@ -2,11 +2,62 @@
 #include "ref_counter.hpp"
 
 
+void my_string_test1();
+void my_string_test2();
+
+void ref_counter_int_test();
+void ref_counter_my_string_test1();
+void ref_counter_my_string_test2();
+
+
 int main() 
 {  
-    //int ref_counter tests
+    //my_string_test1();
+    //my_string_test2();
 
-    /*ref_counter<int> i(new int(55));
+    //ref_counter_int_test();
+
+    //ref_counter_my_string_test1();
+    ref_counter_my_string_test2();
+}
+
+void my_string_test1()
+{
+    my_string s("Hello world");
+    s.print();
+    {
+        my_string t = s;
+        s.print();
+        t.print();
+        cout << s.getChar(1) << endl;
+        s.print();
+        t.print();
+    }
+    s.setChar(1,'E');
+    s.print();
+}
+
+void my_string_test2()
+{
+    my_string s("First object");
+    s.print();
+    {
+        my_string t("Second object");
+        s.print();
+        t.print();
+        s = t;
+        s.print();
+        t.print();
+        s = s;
+        s = t;
+    }
+    s.print();
+}
+
+//int ref_counter test
+void ref_counter_int_test()
+{
+    ref_counter<int> i(new int(55));
     cout << *i;
     i.printRefCount();
     {
@@ -33,14 +84,13 @@ int main()
     i.printRefCount();
     *i = 40;
     cout << *i;
-    i.printRefCount();*/
+    i.printRefCount();
+}
 
-
-
-
-    //my_string ref_counter tests
-
-    /*ref_counter<my_string> s(new my_string("Hello world"));
+// ref_counter my_string tests
+void ref_counter_my_string_test1()
+{
+    ref_counter<my_string> s(new my_string("Hello world"));
     s->print();
     s.printRefCount();
     {
@@ -57,9 +107,11 @@ int main()
     }
     s->setChar(1, 'E');
     s->print();
-    s.printRefCount();*/
+    s.printRefCount();
+}
 
-
+void ref_counter_my_string_test2()
+{
     ref_counter<my_string> s(new my_string("First object"));
     s->print();
     s.printRefCount();
@@ -79,37 +131,4 @@ int main()
     }
     s->print();
     s.printRefCount();
-
-
-
-
-    //my_string tests
-
-    /*my_string s("Hello world");
-    s.print();
-    {
-        my_string t = s;
-        s.print();
-        t.print();
-        cout << s.getChar(1) << endl;
-        s.print();
-        t.print();
-    }
-    s.setChar(1,'E');
-    s.print();*/
-
-    /*my_string s("First object");
-    s.print();
-    {
-        my_string t("Second object");
-        s.print();
-        t.print();
-        s = t;
-        s.print();
-        t.print();
-        s = s;
-        s = t;
-    }
-    s.print();*/
-
 }
